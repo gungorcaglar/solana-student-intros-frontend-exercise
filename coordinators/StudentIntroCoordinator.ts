70cbe1af50b6ad9dd284876a619ad6101d0d027a
Caglar <gungorcaglar@hotmail.com.tr>
2022-11-27 19:49:53 +0300

    solana-student-intros-frontend

--- coordinators/StudentIntroCoordinator.ts
+++ coordinators/StudentIntroCoordinator.ts
@@ -2,7 +2,7 @@ import bs58 from 'bs58'
 import * as web3 from '@solana/web3.js'
 import { StudentIntro } from '../models/StudentIntro'
 
-const STUDENT_INTRO_PROGRAM_ID = 'HdE95RSVsdb315jfJtaykXhXY478h53X6okDupVfY9yf'
+const STUDENT_INTRO_PROGRAM_ID = 'FFQEGDCRHNAhxbyyDEuwTbuzMPSccVjg2RFNLeEY3zZx'
 
 export class StudentIntroCoordinator {
     static accounts: web3.PublicKey[] = []
